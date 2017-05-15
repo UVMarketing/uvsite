38e0e06e5bfc6eddfa3c0cb44ac6d291077d64dd
Luis Iván García Luna <lgarcia@vertixit.com>
2017-05-15 16:29:05 -0500

    Skeleton
    Added new landing page

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="sytlesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+	<title>UV Marketing</title>
+</head>
+<style type="text/css">
+	html{font-family: 'Roboto', sans-serif;}
+</style>
+<body>
+<!-- NAV -->
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo"><img src="https://res.cloudinary.com/itesm-tam/image/upload/c_scale,h_80/v1494883514/logo_uv_pagina-01_f7gmn4.png"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#!">QUIENES SOMOS</a></li>
+        <li><a href="#!">SERVICIOS</a></li>
+        <li><a href="#!">POR QUÉ UV MARKETING</a></li>
+        <li><a href="#!">CONTACTO</a></li>
+      </ul>
+    </div>
+  </nav>
+<!-- /NAV -->
+<!-- HOME -->
+<section id="home">
+	<div class="container">
+		<div class="row">
+			<div class="col s4 m6">
+				<img src="#!" alt="Lupa">
+			</div>
+			<div class="col s8 m6 deep-purple-text">
+				<h1>¿clientes?</h1>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- /HOME -->
+
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+</body>
+</html>
