edb52032b790977dbb5ab7d6e69b683fb746073b
danxmc <danx_97@hotmail.com>
2017-05-15 19:59:10 -0500

    Added Nav and Home sections inside body

--- index.php
+++ index.php
@@ -1,43 +1,56 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<link rel="sytlesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
 	<title>UV Marketing</title>
 </head>
 <style type="text/css">
-	html{font-family: 'Roboto', sans-serif;}
+	html {
+		font-family: 'Roboto', sans-serif;
+		background-image: url("https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/fondo1-01.png");
+		background-color: purple;
+	}
+
+	.purple {
+		color: #351F39!important
+	}
 </style>
+
 <body>
-<!-- NAV -->
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo"><img src="https://res.cloudinary.com/itesm-tam/image/upload/c_scale,h_80/v1494883514/logo_uv_pagina-01_f7gmn4.png"></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="#!">QUIENES SOMOS</a></li>
-        <li><a href="#!">SERVICIOS</a></li>
-        <li><a href="#!">POR QUÉ UV MARKETING</a></li>
-        <li><a href="#!">CONTACTO</a></li>
-      </ul>
-    </div>
-  </nav>
-<!-- /NAV -->
-<!-- HOME -->
-<section id="home">
-	<div class="container">
-		<div class="row">
-			<div class="col s4 m6">
-				<img src="#!" alt="Lupa">
-			</div>
-			<div class="col s8 m6 deep-purple-text">
-				<h1>¿clientes?</h1>
+	<!-- NAV -->
+	<nav>
+		<div class="nav-wrapper purple">
+			<a href="https://uvmarketing.com.mx/" class="brand-logo"><img src="https://res.cloudinary.com/itesm-tam/image/upload/c_scale,h_80/v1494883514/logo_uv_pagina-01_f7gmn4.png"></a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="#!">QUIENES SOMOS</a></li>
+				<li><a href="#!">SERVICIOS</a></li>
+				<li><a href="#!">POR QUÉ UV MARKETING</a></li>
+				<li><a href="#!">CONTACTO</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- /NAV -->
+	<!-- HOME -->
+	<section id="home">
+		<div class="container">
+			<div class="row valign-wrapper">
+				<div class="col s4 left-align">
+					<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/lupa.png" alt="Lupa" class="responsive-img">
+				</div>
+				<div class="col s6 offset-s2 white-text">
+					<h1 style="font-size:6em"><b>¿clientes?</b></h1>
+					<h3 class="left-align">ellos te buscan, nosotros los encontramos.</h3>
+					<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/raya-01.png">
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
-<!-- /HOME -->
+	</section>
+	<!-- /HOME -->
 
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 </body>
+
 </html>
