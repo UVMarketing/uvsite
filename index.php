1ac89182ff380ce24c86b1e79870fe3a4685550c
danxmc <danx_97@hotmail.com>
2017-05-18 17:29:44 -0500

    centeres home, corrected colors

--- index.php
+++ index.php
@@ -9,12 +9,15 @@
 <style type="text/css">
 	html {
 		font-family: 'Roboto', sans-serif;
-		/*background-image: url("https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/fondo1-01.png");*/
 		background-color: white;
 	}
 
 	.navhome {
 		background-image: url("https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/fondo1-01.png");
+	}
+
+	#home {
+		height: 95vh;
 	}
 
 	h2 {
@@ -31,6 +34,12 @@
 		border-bottom: solid 3px;
 	}
 
+	.cyan,
+	.cyan-text {
+		color: #709FB0 !important;
+	}
+
+	.purple,
 	.purple-text {
 		color: #351F39 !important;
 	}
@@ -63,20 +72,27 @@
 
 	.btn {
 		text-transform: none;
+		font-size: 19px;
+		font-stretch: condensed;
 	}
 
 	.col.s4>.btn {
 		width: 60%;
 		border-radius: 15px;
 	}
+
+	nav {
+		height: 80px;
+		line-height: 80px;
+	}
 </style>
 
 <body>
 	<div class="navhome">
 		<!-- NAV -->
-		<nav class="nav-extended transparent z-depth-0" style="height: 80px;line-height: 80px;">
+		<nav class="transparent z-depth-0">
 			<div class="nav-wrapper">
-				<a href="https://uvmarketing.com.mx/" class="brand-logo"><img src="https://res.cloudinary.com/itesm-tam/image/upload/c_scale,h_80/v1494883514/logo_uv_pagina-01_f7gmn4.png"></a>
+				<a href="https://uvmarketing.com.mx/" class="brand-logo"><img src="https://res.cloudinary.com/itesm-tam/image/upload/c_scale,h_80/v1494883514/logo_uv_pagina-01_f7gmn4.png "></a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
 					<li><a href="#quienessomos" class="sliding-u-l-r">QUIENES SOMOS</a></li>
 					<li><a href="#servicios" class="sliding-u-l-r">SERVICIOS</a></li>
@@ -86,15 +102,15 @@
 			</div>
 		</nav>
 		<!-- /NAV -->
-		<br><br><br><br><br>
+
 		<!-- HOME -->
-		<section id="home">
+		<section id="home" class="valign-wrapper">
 			<div class="container">
-				<div class="row valign-wrapper">
-					<div class="col s4 offset-s1 left-align">
+				<div class="row">
+					<div class="col s4 offset-s1">
 						<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/lupa.png" alt="Lupa" class="responsive-img">
 					</div>
-					<div class="col s5 offset-s1 white-text">
+					<div class="col s6 offset-s1 white-text">
 						<h1 style="font-size:6em"><b>¿clientes?</b></h1>
 						<h3 class="left-align">ellos te buscan, nosotros los encontramos.</h3>
 						<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/raya-01.png">
@@ -103,40 +119,41 @@
 			</div>
 		</section>
 		<!-- /HOME -->
-		<br><br><br><br><br>
 	</div>
 	<!-- QUIENES SOMOS -->
-	<section id="quienessomos">
-		<div class="container">
-			<div class="row left-align">
-				<div class="col s12">
-					<h2 class="purple-text">QUIENES SOMOS</h2>
+	<section id="quienessomos ">
+		<div class="container ">
+			<div class="row left-align ">
+				<div class="col s12 ">
+					<h2 class="purple-text ">QUIENES SOMOS</h2>
 				</div>
 			</div>
-			<div class="row left-align">
-				<div class="col s5">
+			<br>
+			<div class="row left-align ">
+				<div class="col s5 ">
 					<p>Jóvenes profesionales dedicados a crear, desarrollar e implementar estrategias de marketing que vayan <b>más allá de lo visible.</b></p>
-					<br>
+					<br><br>
 					<p>Combinamos nuestras 3 áreas de trabajo para poder generar acciones que aumenten la probabilidad de concretar ventas de nuestros clientes.</p>
 				</div>
 			</div>
 		</div>
 	</section>
 	<!-- /QUIENES SOMOS -->
+	<br><br><br>
 	<!-- SERVICIOS -->
-	<section id="servicios">
-		<div class="container">
-			<div class="row left-align">
-				<div class="col s12">
-					<h2 class="cyan-text text-darken-1"><b>SERVICIOS</b></h2>
+	<section id="servicios ">
+		<div class="container ">
+			<div class="row left-align ">
+				<div class="col s12 ">
+					<h2 class="cyan-text ">SERVICIOS</h2>
 				</div>
 			</div>
 		</div>
-		<div class="row valign-wrapper cyan darken-1">
-			<div class="col s4">
-				<img src="" alt="img-papers">
+		<div class="row valign-wrapper cyan " style="background-color: #709FB0 !important ">
+			<div class="col s4 ">
+				<img src=" " alt="img-papers ">
 			</div>
-			<div class="col s4 white-text">
+			<div class="col s4 white-text ">
 				<p><b>Web Search Engine Strategy</b></p>
 				<p>Llega a las personas que están buscando negocios como el tuyo.</p>
 				<p><b>Desarrollo Web</b></p>
@@ -146,30 +163,30 @@
 				<p><b>Identidad Corporativa</b></p>
 				<p>Transmite lo que es en realidad tu negocio a través de tu imagen.</p>
 			</div>
-			<div class="col s4 center-align">
-				<a class="waves-effect waves white cyan-text text-darken-1 btn"><b>Conoce más</b></a>
+			<div class="col s4 center-align ">
+				<a class="waves-effect waves white cyan-text btn ">Conoce más</a>
 			</div>
 		</div>
 	</section>
 	<!-- /SERVICIOS -->
 	<!-- CLIENTES -->
-	<section id="clientes">
-		<div class="row valign-wrapper">
-			<h2 class="col s2 purple-text">CLIENTES</h2>
+	<section id="clientes ">
+		<div class="row valign-wrapper ">
+			<h2 class="col s2 purple-text ">CLIENTES</h2>
 		</div>
 
-		<div class="row">
+		<div class="row ">
 
-			<div class="col s4 m5">
-				<div class="card-panel purple">
-					<span class="white-text">Glass&Glass
+			<div class="col s4 m5 ">
+				<div class="card-panel purple ">
+					<span class="white-text ">Glass&Glass
           </span>
 				</div>
 			</div>
 
-			<div class="col s4 m5">
-				<div class="card-panel purple">
-					<span class="white-text">Escape rooms
+			<div class="col s4 m5 ">
+				<div class="card-panel purple ">
+					<span class="white-text ">Escape rooms
           </span>
 				</div>
 			</div>
