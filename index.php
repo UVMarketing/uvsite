7b9eef0671f6b291ce6c04d7f0450c7901fbeaf0
danxmc <danx_97@hotmail.com>
2017-05-25 21:11:46 -0500

    styling

--- index.php
+++ index.php
@@ -9,14 +9,15 @@
 		<meta property="og:title" content="UV Marketing" />
 		<meta property="og:type" content="website" />
 		<meta property="og:url" content="https://uvmarketing.com.mx/" />
-		<meta property="og:image" content="https://res.cloudinary.com/duhncjntk/image/upload/c_lpad,h_630,w_1200/v1495752080/uvsite/Logonuevomorado-01_nzhhdp.png" />
+		<meta property="og:image" content="https://res.cloudinary.com/duhncjntk/image/upload/c_lpad,h_630,w_1200/v1495752080/LogoNuevo.png" />
+		<meta property="og:description" content="Jóvenes profesionales dedicados a crear, desarrollar e implementar estrategias de marketing que vayan más allá de lo visible.">
 		<meta name="charset" content="UTF-8">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100,400,700" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 		<link rel="stylesheet" href="style/main.css">
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="https://res.cloudinary.com/duhncjntk/image/upload/v1495764414/uvsite/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="https://res.cloudinary.com/duhncjntk/image/upload/v1495764414/uvsite/favicon.ico" type="image/x-icon">
 		<title>UV Marketing</title>
 	</head>
 
@@ -44,8 +45,8 @@
 						<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/lupa.png" alt="Lupa" class="responsive-img">
 					</div>
 					<div class="col s6 offset-s1 white-text">
-						<h1 style="font-size:6em"><b>¿clientes?</b></h1>
-						<h3 class="left-align">ellos te buscan, nosotros los encontramos.</h3>
+						<h1 style="font-size:6em"><strong>¿clientes?</strong></h1>
+						<h3 class="left-align" style="font-weight:100;">ellos te buscan, nosotros los encontramos.</h3>
 						<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/raya-01.png">
 					</div>
 				</div>
@@ -115,11 +116,11 @@
 					</div>
 				</div>
 			</div>
-			<div class="row valign-wrapper cyan" style="background-color: #709FB0 !important;margin-bottom:-1em; position: relative;">
+			<div class="row valign-wrapper cyan" style="background-color: #709FB0!important;margin-bottom:-1em;position: relative;">
 				<span class="bgtext">UV</span>
 				<div class="col m4 bgimg" style="background-image:url('https://res.cloudinary.com/duhncjntk/image/upload/q_30/v1495483008/uvsite/pexels-photo-191830.jpg');">
 				</div>
-				<div class="col m4 white-text animatable fadeInUp serv" style="margin: 5em 0em 5em 5em; font-weight:100;">
+				<div class="col m4 white-text animatable fadeInUp serv" style="padding-left:5em;font-weight:100;">
 					<p><b>Web Search Engine</b><br>Llega a las personas que están buscando negocios como el tuyo.</p>
 					<p><b>Desarrollo Web</b><br>Coloca tu negocio en la red y brinda la información y servicios que necesitan tus clientes.</p>
 					<p><b>Campañas Publicitarias - UV Technologies</b><br>¿Deseas lanzar un nuevo producto o posicionar tu marca? Imagina usar las últimas tendencias en tecnología para hacerlo.</p>
@@ -132,7 +133,7 @@
 		</section>
 		<!-- /SERVICIOS -->
 		<!-- CLIENTES -->
-		<section id="clientes" style="z-index:1" class="section scrollspy">
+		<section id="clientes" class="section scrollspy">
 			<div class="row valign-wrapper">
 				<div class="col m9">
 					<div class="carousel animatable fadeIn">
@@ -140,7 +141,7 @@
 				foreach ($clients as $key => $value) {
 					echo "<a href='#!' class='carousel-item'>
 						<div class='col m12'>
-							<div class='card-panel hoverable purple' style='height:35vh;width:30vw'>
+							<div class='card-panel hoverable light-purple' style='height:35vh;width:30vw'>
 								<div class='card-image col m12'>
 									<img src='$value->picture' class='right' style='img-responsive'>
 								</div>
@@ -165,10 +166,9 @@
 		<section id="contact" class="white-text purple home section scrollspy">
 			<div class="container">
 				<div class="row" style="margin-bottom:-1em;margin-top:-1em">
-					<div class="col l7 s12 home">
-						<h3>CONTACTANOS</h3>
-						<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/raya-01.png">
-						<p>Este es el primer paso para iniciar una estrategia <i>ultravioleta</i>.Déjanos tus datos y nos comunicaremos a la brevedad posible.</p>
+					<div class="col l6 s12 home">
+						<h2>CONTACTANOS</h2>
+						<p>Este es el primer paso para iniciar una estrategia <i>ultravioleta</i>.<br>Déjanos tus datos y nos comunicaremos a la brevedad posible.</p>
 						<form class="col s12">
 							<div class="row">
 								<div class="input-field col s10">
@@ -196,49 +196,46 @@
 						</label>
 								</div>
 								<div class="input-field col s10">
-									<input type="submit" value="Enviar Mensaje" class="btn waves-effect deep-purple animated infinite pulse">
+									<input type="submit" value="Enviar Mensaje" class="btn waves-effect light-purple animated infinite pulse">
 									<div id="status">
 									</div>
 								</div>
 							</div>
 						</form>
 					</div>
-					<div class="col l5 s12 purple">
-						<h3 class="white-text">NUESTRA OFICINA</h3>
-						<img src="https://res.cloudinary.com/duhncjntk/image/upload/v1494893710/uvsite/raya-01.png">
-						<p>Avenida Universidad de Wisconsin #208 Int. J Colonia Universidad Sur, CP 89109 Tampico, Tamaulipas, México.
-						</p>
-						<p>Teléfono
-							<b>(833)</b>217 06 22</p>
-						<p>Correo electrónico
-							<a href="mailto:contacto@uvmarketing.com.mx" class="deep-purple-text">contacto@uvmarketing.com.mx</a></p>
+					<div class="col l6 s12 purple" style="padding-left:5em;">
+						<h2 class="white-text ">NUESTRA OFICINA</h2>
+						<p style="padding-top:5em ">Avenida Universidad de Wisconsin #208 Int. J<br>Colonia Universidad Sur, CP 89109<br>Tampico, Tamaulipas, México.</p>
+						<p style="padding-top:2em ">Teléfono<br> (833) 217 06 22</p>
+						<p style="padding-top:2em ">Correo electrónico<br>
+							<a href="mailto:contacto@uvmarketing.com.mx " class="white-text ">contacto@uvmarketing.com.mx</a></p>
 					</div>
 				</div>
 			</div>
 		</section>
 		<!-- /CONTACT -->
 		<!-- FOOTER -->
-		<footer class="page-footer grey darken-2 animatable fadeIn">
-			<div class="container">
-				<div class="row">
-					<div class="col l12 s12 center">
-						<h5 class="white-text small">UV Marketing <small class="grey-text text-lighten-4">Mercadotecnia Digital</small></h5>
+		<footer class="page-footer grey darken-2 animatable fadeIn ">
+			<div class="container ">
+				<div class="row ">
+					<div class="col l12 s12 center ">
+						<h5 class="white-text small ">UV Marketing <br><small class="grey-text text-lighten-4 ">Mercadotecnia Digital</small></h5>
 					</div>
 				</div>
 			</div>
-			<div class="footer-copyright">
-				<div class="container">
+			<div class="footer-copyright ">
+				<div class="container ">
 					©
 					<?php echo date('Y') ?> Copyright UV Marketing
-					<a class="grey-text text-lighten-4 right" href="mailto:contacto@uvmarketing.com.mx">contacto@uvmarketing.com.mx</a>
+					<a class="grey-text text-lighten-4 right " href="mailto:contacto@uvmarketing.com.mx ">contacto@uvmarketing.com.mx</a>
 				</div>
 			</div>
 		</footer>
 		<!-- /FOOTER -->
-		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
-		<script src="assets/app.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js "></script>
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js "></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js "></script>
+		<script src="assets/app.js "></script>
 	</body>
 
 	</html>
