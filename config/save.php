3a3987b1321c12b489afca0ed033aea368f4c710
Francisco Tomás Zapata Guerrero <tomaszapata@iMacdeFrancisco.lan>
2017-06-05 17:02:50 -0500

    config update

--- config/save.php
+++ config/save.php
@@ -1,19 +1,27 @@
 <?php
+  session_start();
   require 'db.php';
   $contact = R::dispense('contacts');
-  $contact->name = $_POST['name'];;
-  $contact->email = $_POST['email'];
-  $contact->phone = $_POST['phone'];
-  $contact->message = $_POST['message'];
-  $id = R::store($contact);
+  if (isset($_POST['name']) AND isset($_POST['email'])) {
+    $contact->name = $_POST['name'];;
+    $contact->email = $_POST['email'];
+    $contact->phone = $_POST['phone'];
+    $contact->message = $_POST['message'];
+    $id = R::store($contact);
 
-  $title = 'UV Marketing | Contacto';
-  $message = '<html><body>';
-  $message .= '<h1>Hola '.$contact->name.'</h1>'
-  .'<h3>Gracias por contactarnos<br>'
-  .'Nos comunicaremos en la brevedad contigo.</h3>';
-  $message .='</body></html>';
-  $headers = 'MIME-Version: 1.0'."\r\n".'Content-type: text/html; charset=UTF-8'."\r\n";
-  $headers .= 'From: contacto@uvmarketing.com.mx'."\r\n".'Reply-To: contacto@uvmarketing.com.mx'."\r\n".'X-Mailer: PHP/'.phpversion();
-  mail($contact->email,$title,$message,$headers);
+    $title = 'UV Marketing | Contacto';
+    $message = '<html><body>';
+    $message .= '<h1>Hola '.$contact->name.'</h1>'
+    .'<h3>Gracias por contactarnos<br>'
+    .'Nos comunicaremos en la brevedad contigo.</h3>';
+    $message .='</body></html>';
+    $headers = 'MIME-Version: 1.0'."\r\n".'Content-type: text/html; charset=UTF-8'."\r\n";
+    $headers .= 'From: contacto@uvmarketing.com.mx'."\r\n".'Reply-To: contacto@uvmarketing.com.mx'."\r\n".'X-Mailer: PHP/'.phpversion();
+    mail($contact->email,$title,$message,$headers);
+    $_SESSION['message'] = $contact->name.', Gracias por enviar tu mensaje';
+    header('location:../index.php');
+  }else {
+    $_SESSION['message'] = 'Error al enviar tu mensaje';
+    header('location:../index.php');
+  }
  ?>
